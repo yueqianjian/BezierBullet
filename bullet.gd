06c30ad997669dab03dd811ee5146b701da5cc71
yueqianjian <yueqianjian@gmail.com>
2023-06-19 21:17:46 +0800

    update t

--- bullet.gd
+++ bullet.gd
@@ -1,20 +1,14 @@
 extends Node2D
 
-var speed = 500.0
+var speed = 700.0
 var direction = Vector2.ZERO
-
-var start_point = Vector2.ZERO
-var target_point = Vector2.ZERO
-var start_control_point = Vector2.ZERO
-var target_control_point = Vector2.ZERO
 var next_point = Vector2.ZERO
-var t = 0.0
+var current_time = 0.0
 
 func _ready():
 	look_at(get_global_mouse_position())
 	direction = (get_global_mouse_position() - global_position).normalized()
 	next_point = global_position
-	start_point = global_position
 
 
 func set_next_point(delta):
@@ -22,8 +16,11 @@ func set_next_point(delta):
 	if !enemy:
 		return
 	
-	t = min((speed * delta / 500) + t, 1) 
-	start_control_point = direction * (global_position.distance_to(enemy.global_position) / 0.3)
+	current_time += delta	
+	var distance = global_position.distance_to(enemy.global_position)
+	var all_time = distance / speed
+	var t = min(current_time / all_time, 1)
+	var start_control_point = direction * speed * 1.7
 	next_point = global_position.bezier_interpolate(start_control_point, enemy.global_position, enemy.global_position, t)
 
 
